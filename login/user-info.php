6980541fe6724de95f29c38362a8ef161ec9d4b6
Drishti Malavade <102899430+DrishtiMalavade@users.noreply.github.com>
2024-03-17 12:13:32 +0530

    user-info.php

--- login/user-info.php
+++ login/user-info.php
@@ -5,7 +5,82 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="user-info.css">
     <title>USER</title>
+    
+    <style>
+        
+@import url('https://fonts.googleapis.com/css2?family=Fredericka+the+Great&family=Gloria+Hallelujah&family=Poppins:wght@300;600&display=swap');
 
+@font-face {
+    font-family: 'Dream Avenue';
+    src: url('../homepage/Dream-Avenue.ttf') format('truetype');
+}
+    
+*{
+    background-color: black;
+    color: aliceblue;
+}
+
+.head{
+        text-align: center;
+        padding: 50px;
+        font-family: 'Dream Avenue'; 
+    }
+
+.message {
+    background-color: aliceblue;
+    color: black;
+    font-family: 'Gloria Hallelujah', cursive;
+    text-align: center;
+    margin: 5% 20% 10%; /* Adjust the percentage values as needed */
+    padding: 5%; /* You can also use percentage for padding */
+    }
+
+body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+header {
+    font-family: 'Dream Avenue';  
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: black;
+    color: #fff;
+    padding: 15px;
+}
+
+.brand {
+    font-size: 1.5em;
+}
+
+.nav-links {
+    display: flex;
+}
+
+.nav-links a {
+    color: #fff;
+    text-decoration: none;
+    margin: 0 15px;
+    position: relative;
+}
+
+.nav-links a:after {
+    content: '';
+    display: block;
+    height: 2px;
+    width: 0;
+    background-color: #fff;
+    transition: width 0.3s;
+}
+
+.nav-links a:hover:after {
+    width: 100%;
+}
+
+    </style>
+    
 </head>
 <body>
 
